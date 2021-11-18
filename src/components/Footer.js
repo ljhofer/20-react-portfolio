3e92595a0afbbb0d8d816bb8193e895e6247036f
Laura Hofer <ljhofer@gmail.com>
2021-11-18 10:00:59 -0600

    Added images for GitHub and Twitter in footer page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import githubImage from '../images/github.png';
+import twitterImage from '../images/twitter.png'
 
 
 const styles = {
@@ -6,13 +8,19 @@ const styles = {
       textAlign: 'center',
       margin: 25
     },
+    img: {
+      margin: 25,
+    }
   };
 
 export default function Footer () {
 
     return (
-
+      <>
         <h2 style={styles.h2}> Created by Laura Hofer</h2>
+        <a href='https://github.com/ljhofer' target="_blank"><img src={githubImage} alt='GitHub logo' style={styles.img}/></a>
+        <a href='https://www.twitter.com' target="_blank"><img src={twitterImage} alt='GitHub logo' style={styles.img}/></a>
+      </>  
     )
 };
 
