2fd043b4d53d2e58a676d09b69d2e444b7195c4c
Laura Hofer <ljhofer@gmail.com>
2021-11-21 08:50:55 -0600

    Fixed typos in portofolio cards, added links to portfolio images, added a screen shot of my resume

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -36,10 +36,10 @@ export default function Portfolio() {
       <h1 style={styles.h1}>Portfolio</h1>
       <section style={styles.section} className='row' id='projects'>
         <div style={styles.card} className="card col-10 col-md-5 col-lg-3" >
-            <img className="card-img-top" src={catImage} alt="Cat looking at a computer" />
+            <a href="https://captionmeme.herokuapp.com/" target="_blank" rel="noreferrer"><img className="card-img-top" src={catImage} alt="Cat looking at a computer" /></a>
             <div className="card-body">
               <h5 className="card-title">CaptionMe(me)</h5>
-              <p style={styles.p} className="card-text">This app allows users to caption an image to create a memet. I coded the routes, models, seeds, event handlers, server, and index files. I also served as Project Manager.</p>
+              <p style={styles.p} className="card-text">This app allows users to caption an image to create a meme. I coded the routes, models, seeds, event handlers, server, and index files. I also served as Project Manager.</p>
             </div>
             <div className="card-body">
               <a href="https://captionmeme.herokuapp.com/" style={styles.a} className="card-link" target="_blank" rel="noreferrer">Deployed Code</a>
@@ -48,10 +48,10 @@ export default function Portfolio() {
         </div>
 
         <div style={styles.card}className="card col-10 col-md-5 col-lg-3">
-            <img className="card-img-top" src={movieImage} alt="Film equipment" />
+            <a href="https://cleadi.github.io/nextflix/" target="_blank" rel="noreferrer"><img className="card-img-top" src={movieImage} alt="Film equipment" /></a>
             <div className="card-body">
               <h5 className="card-title">Nextflix Movie Favorites</h5>
-              <p style={styles.p} className="card-text">This app allowd the user to search movies and add titles to a favorites list. I coded the JavaScript and jQuery for the landing page and serves as Project Manager.</p>
+              <p style={styles.p} className="card-text">This app allows the user to search movies and add titles to a favorites list. I coded the JavaScript and jQuery for the landing page and served as Project Manager.</p>
             </div>
             <div className="card-body">
               <a href="https://cleadi.github.io/nextflix/" style={styles.a}className="card-link" target="_blank" rel="noreferrer">Deployed Code</a>
@@ -60,10 +60,10 @@ export default function Portfolio() {
         </div>
 
         <div style={styles.card} className="card col-10 col-md-5 col-lg-3">
-            <img className="card-img-top" src={weatherImage} alt="Foggy landscape" />
+            <a href="https://ljhofer.github.io/06-server-side-apis-weather-dashboard/" target="_blank" rel="noreferrer"><img className="card-img-top" src={weatherImage} alt="Foggy landscape" /></a>
             <div className="card-body">
               <h5 className="card-title">Weather Dashboard</h5>
-              <p style={styles.p} className="card-text">The page allows use to search by city and dispalys current weather and a five-day forecast.</p>
+              <p style={styles.p} className="card-text">The page allows the user to search by city and displays current weather and a five-day forecast.</p>
             </div>
             <div className="card-body">
               <a href="https://ljhofer.github.io/06-server-side-apis-weather-dashboard/" style={styles.a} className="card-link" target="_blank" rel="noreferrer">Deployed Code</a>
@@ -72,10 +72,10 @@ export default function Portfolio() {
         </div>
 
         <div style={styles.card} className="card col-10 col-md-5 col-lg-3">
-            <img className="card-img-top" src={blogImage} alt="Foggy landscape" />
+            <a href="https://ljhofer-tech-blog.herokuapp.com/" target="_blank" rel="noreferrer"><img className="card-img-top" src={blogImage} alt="Foggy landscape" /></a>
             <div className="card-body">
               <h5 className="card-title">Tech Blog</h5>
-              <p style={styles.p} className="card-text">The page allows use to search by city and dispalys current weather and a five-day forecast.</p>
+              <p style={styles.p} className="card-text">This Tech Blog allows users to see all blog posed and comment created on the page. Onece logged in users can comment on posts, as well as create, update, and delete their own posts.</p>
             </div>
             <div className="card-body">
               <a href="https://ljhofer-tech-blog.herokuapp.com/" style={styles.a} className="card-link" target="_blank" rel="noreferrer">Deployed Code</a>
@@ -84,10 +84,10 @@ export default function Portfolio() {
         </div>
 
         <div style={styles.card} className="card col-10 col-md-5 col-lg-3">
-            <img className="card-img-top" src={employeeImage} alt="Table with laptops, headphones, snacks, and drinks" />
+            <a href="https://github.com/ljhofer/sql-employee-tracker" target="_blank" rel="noreferrer"><img className="card-img-top" src={employeeImage} alt="Table with laptops, headphones, snacks, and drinks" /></a>
             <div className="card-body">
               <h5 className="card-title">Employee Tracker</h5>
-              <p style={styles.p} className="card-text">This command line app allows used to create deparments, roles, and employees in their database. It also allows them to update and delete as necessary.</p>
+              <p style={styles.p} className="card-text">This command line app allows users to create deparments, roles, and employees in their database. It also allows them to update and delete as necessary.</p>
             </div>
             <div style={styles.singleLink} className="card-body">
               <a href="https://github.com/ljhofer/sql-employee-tracker" style={styles.a} className="card-link" target="_blank" rel="noreferrer">GitHub Repo</a>
@@ -95,7 +95,7 @@ export default function Portfolio() {
         </div>
         
         <div className="card col-10 col-md-5 col-lg-3">
-            <img className="card-img-top" src={emailImage} alt="At symbol" />
+            <a href="https://gist.github.com/ljhofer/d8682f596f60a59e280b1f8f07cf43b2" target="_blank" rel="noreferrer"><img className="card-img-top" src={emailImage} alt="At symbol" /></a>
             <div className="card-body">
               <h5 className="card-title">Email Validation Gist</h5>
               <p style={styles.p} className="card-text">I wrote a gist explaining a regular expression for validating emails. It details the different sections of the regex and gives examples of their uses.</p>
